f8c6a29e8827d945d93a36254b916ff00b6069bd
Jacco Meijer <jacco@jaccomeijer.nl>
2024-02-14 16:41:56 +0100

    chore: remove story layout from export

--- src/stories/story-layout.jsx
+++ src/stories/story-layout.jsx
@@ -1,7 +1,7 @@
-import { Head } from './head.jsx'
-import { StoryMenu } from '../components/navigation/story-menu/story-menu.jsx'
-import { getMenuItems } from './get-menu-items.js'
-import favicon from '../../assets/favicon/favicon.ico'
+import { Head } from '../package/layouts/head.jsx'
+import { StoryMenu } from '../package/components/navigation/story-menu/story-menu.jsx'
+import { getMenuItems } from '../package/layouts/get-menu-items.js'
+import favicon from '../assets/favicon/favicon.ico'
 
 export const StoryLayout = props => {
   const mainMenu = getMenuItems({
