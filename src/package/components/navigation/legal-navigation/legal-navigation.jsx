7390582d99bb7b163c6a77e5cd9be590688e2069
Jacco Meijer <jacco@jaccomeijer.nl>
2024-02-15 17:01:01 +0100

    fix: footer alignment

--- src/package/components/navigation/legal-navigation/legal-navigation.jsx
+++ src/package/components/navigation/legal-navigation/legal-navigation.jsx
@@ -1,12 +1,16 @@
-import ceCss from './legal-navigation.ce.css'
+import ceCss from '../navigation.ce.css'
 import ceJs from './legal-navigation.ce.js'
 import globalCss from '../../../css/global.bundle.css'
+import { getAction } from '../../../layouts/get-action.js'
 
 export const LegalNavigation = props => (<legal-navigation>
   <template shadowrootmode="open">
     <nav id="legal-navigation">
       <p variant="xs">{props.globals.metadata.copyright}</p>
-      <a variant="xs">{props.globals.metadata.vendor}<sup>{props.globals.metadata.version}</sup></a>
+      <a 
+        variant="xs"
+        href={getAction({ type: 'href', globals: props.globals, url: props.globals.metadata.url })}
+      >{props.globals.metadata.vendor}<sup>{props.globals.metadata.version}</sup></a>
     </nav>
     <link rel="stylesheet" type="text/css" href={`${props.globals.baseUrl}${props.globals.assetUrl}${globalCss}`} />
     <link rel="stylesheet" type="text/css" href={`${props.globals.baseUrl}${props.globals.assetUrl}${ceCss}`} />
